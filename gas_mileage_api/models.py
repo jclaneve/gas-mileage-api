73046f6337563d023f73a47e4c73155d648bef14
Joseph Laneve <joseph.c.laneve@gmail.com>
2020-12-07 16:45:38 -0500

    Enable Django admin for user profile model

--- gas_mileage_api/models.py
+++ gas_mileage_api/models.py
@@ -7,28 +7,28 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, first_name, last_name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address.')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-def create_superuser(self, email, name, password):
-    """Create and save a new superuser with given details"""
-    user = self.create_user(email, name, password)
+    def create_superuser(self, email, first_name, last_name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, first_name, last_name, password)
 
-    user.is_superuser = True
-    user.is_staff = True
-    user.save(using=self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-    return user
+        return user
     
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
@@ -42,7 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
